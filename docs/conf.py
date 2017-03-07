50479301a7c20810a0dc036342448b7eb8d5166f
Kyle Wilcox <wilcox.kyle@gmail.com>
2017-03-07 00:24:57 -0500

    Adding github page/badge to docs sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,12 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'description': 'A python framework for working with met-ocean data',
+    'github_user': 'pyoceans',
+    'github_repo': 'pocean-core',
+    'github_button': 'true',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
