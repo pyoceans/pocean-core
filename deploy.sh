42065487b5c37702d2cae7071361c3892f1d3c39
Kyle Wilcox <kyle@axiomdatascience.com>
2017-03-06 21:44:46 -0500

    Clear docs

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-if [ "$TRAVIS_PYTHON_VERSION" == "3.5" ]; then
-    conda install --file docs/requirements.txt
-    mkdocs build --clean --verbose
-    doctr deploy --built-docs=_site --gh-pages-docs .
-fi
