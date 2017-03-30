f2408ccc166102833c1dbbf2b6be34c89ac26ed2
ocefpaf <ocefpaf@gmail.com>
2017-03-30 19:39:56 -0300

    Split test context.

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -ev
 
-if [ "$TRAVIS_PYTHON_VERSION" == "3.5" ]; then
-    cd docs
-    conda install --file requirements.txt
-    sphinx-apidoc -M -f -o api ../pocean ../pocean/tests
-    make html
-    doctr deploy --built-docs=_site/html .
-    cd ..
-fi
+cd docs
+conda install --file requirements.txt
+sphinx-apidoc -M -f -o api ../pocean ../pocean/tests
+make html
+doctr deploy --built-docs=_site/html .
+cd ..
