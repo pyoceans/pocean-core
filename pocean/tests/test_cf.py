d49955abfe4a113060afd20d7ec3ad68ab4b9799
Filipe Fernandes <ocefpaf@gmail.com>
2023-09-05 14:04:40 -0300

    should fix the failing tests

--- pocean/tests/test_cf.py
+++ pocean/tests/test_cf.py
@@ -15,7 +15,7 @@ L.handlers = [logging.StreamHandler()]
 class TestCFDatasetLoad(unittest.TestCase):
 
     def test_load_url(self):
-        ncd = CFDataset.load('http://geoport.whoi.edu/thredds/dodsC/usgs/data2/emontgomery/stellwagen/CF-1.6/ARGO_MERCHANT/1211-AA.cdf')
+        ncd = CFDataset.load('https://geoport.whoi.edu/thredds/dodsC/usgs/data2/emontgomery/stellwagen/CF-1.6/ARGO_MERCHANT/1211-AA.cdf')
         assert omt.is_mine(ncd) is True
         ncd.close()
 
