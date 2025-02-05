86a585f644c685defe6f0b80a9d1889ea1a33bb1
Filipe Fernandes <ocefpaf@gmail.com>
2025-02-05 18:28:22 +0100

    debug

--- pocean/tests/test_utils.py
+++ pocean/tests/test_utils.py
@@ -17,77 +17,80 @@ logger.level = logging.INFO
 logger.handlers = [logging.StreamHandler()]
 
 
-class TestUtils(unittest.TestCase):
-    def setUp(self):
-        self.input_file = os.path.join(os.path.dirname(__file__), "resources/coamps.nc")
+input_file = os.path.join(os.path.dirname(__file__), "resources/coamps.nc")
 
-    def test_single_attr_filter(self):
-        with EnhancedDataset(self.input_file) as nc:
-            grid_spacing_vars = nc.filter_by_attrs(grid_spacing="4.0 km")
+@pytest.fixture
+def load_dataset():
+    nc = EnhancedDataset(input_file)
+    yield nc
+    nc.close()
 
-            x = nc.variables.get("x")
-            y = nc.variables.get("y")
 
-            self.assertEqual(len(grid_spacing_vars), 2)
-            assert x in grid_spacing_vars
-            assert y in grid_spacing_vars
+def test_single_attr_filter(load_dataset):
+    grid_spacing_vars = load_dataset.filter_by_attrs(grid_spacing="4.0 km")
 
-    def test_multiple_attr_filter(self):
-        with EnhancedDataset(self.input_file) as nc:
-            grid_spacing_vars = nc.filter_by_attrs(
-                grid_spacing="4.0 km", standard_name="projection_y_coordinate"
-            )
+    x = load_dataset.variables.get("x")
+    y = load_dataset.variables.get("y")
 
-            y = nc.variables.get("y")
+    assert len(grid_spacing_vars) == 2
+    assert x in grid_spacing_vars
+    assert y in grid_spacing_vars
 
-            self.assertEqual(len(grid_spacing_vars), 1)
-            assert y in grid_spacing_vars
+def test_multiple_attr_filter(load_dataset):
+    grid_spacing_vars = load_dataset.filter_by_attrs(
+        grid_spacing="4.0 km", standard_name="projection_y_coordinate"
+    )
 
-    @pytest.mark.filterwarnings("ignore::UserWarning")
-    def test_generic_masked_bad_min_max_value(self):
-        fid, tpath = tempfile.mkstemp(suffix=".nc", prefix="pocean-test")
-        shutil.copy2(self.input_file, tpath)
+    y = load_dataset.variables.get("y")
 
-        with EnhancedDataset(tpath, "a") as ncd:
-            v = ncd.variables["v_component_wind_true_direction_all_geometries"]
-            v.valid_min = np.float32(0.1)
-            v.valid_max = np.float32(0.1)
-            r = generic_masked(v[:], attrs=ncd.vatts(v.name))
-            rflat = r.flatten()
-            assert rflat[~rflat.mask].size == 0
+    assert len(grid_spacing_vars) == 1
+    assert y in grid_spacing_vars
 
-            # Create a byte variable with a float valid_min and valid_max
-            # to make sure it doesn't error
-            b = ncd.createVariable("imabyte", "b")
-            b.valid_min = 0
-            b.valid_max = np.int16(600)  # this is over a byte and thus invalid
-            b[:] = 3
-            r = generic_masked(b[:], attrs=ncd.vatts(b.name))
-            assert np.all(r.mask == False)  # noqa
+@pytest.mark.filterwarnings("ignore::UserWarning")
+def test_generic_masked_bad_min_max_value():
+    fid, tpath = tempfile.mkstemp(suffix=".nc", prefix="pocean-test")
+    shutil.copy2(input_file, tpath)
 
-            b.valid_min = 0
-            b.valid_max = 2
-            r = generic_masked(b[:], attrs=ncd.vatts(b.name))
-            assert np.all(r.mask == True)  # noqa
+    with EnhancedDataset(tpath, "a") as ncd:
+        v = ncd.variables["v_component_wind_true_direction_all_geometries"]
+        v.valid_min = np.float32(0.1)
+        v.valid_max = np.float32(0.1)
+        r = generic_masked(v[:], attrs=ncd.vatts(v.name))
+        rflat = r.flatten()
+        assert rflat[~rflat.mask].size == 0
 
-            c = ncd.createVariable("imanotherbyte", "f4")
-            c.setncattr("valid_min", b"0")
-            c.setncattr("valid_max", b"9")
-            c[:] = 3
-            r = generic_masked(c[:], attrs=ncd.vatts(c.name))
-            assert np.all(r.mask == False)  # noqa
+        # Create a byte variable with a float valid_min and valid_max
+        # to make sure it doesn't error
+        b = ncd.createVariable("imabyte", "b")
+        b.valid_min = 0
+        b.valid_max = np.int16(600)  # this is over a byte and thus invalid
+        b[:] = 3
+        r = generic_masked(b[:], attrs=ncd.vatts(b.name))
+        assert np.all(r.mask == False)  # noqa
 
-            c = ncd.createVariable("imarange", "f4")
-            c.valid_range = [0.0, 2.0]
-            c[:] = 3.0
-            r = generic_masked(c[:], attrs=ncd.vatts(c.name))
-            assert np.all(r.mask == True)  # noqa
+        b.valid_min = 0
+        b.valid_max = 2
+        r = generic_masked(b[:], attrs=ncd.vatts(b.name))
+        assert np.all(r.mask == True)  # noqa
 
-            c.valid_range = [0.0, 2.0]
-            c[:] = 1.0
-            r = generic_masked(c[:], attrs=ncd.vatts(c.name))
-            assert np.all(r.mask == False)  # noqa
+        c = ncd.createVariable("imanotherbyte", "f4")
+        c.setncattr("valid_min", b"0")
+        c.setncattr("valid_max", b"9")
+        c[:] = 3
+        r = generic_masked(c[:], attrs=ncd.vatts(c.name))
+        assert np.all(r.mask == False)  # noqa
 
-        os.close(fid)
-        if os.path.exists(tpath):
-            os.remove(tpath)
+        c = ncd.createVariable("imarange", "f4")
+        c.valid_range = [0.0, 2.0]
+        c[:] = 3.0
+        r = generic_masked(c[:], attrs=ncd.vatts(c.name))
+        assert np.all(r.mask == True)  # noqa
+
+        c.valid_range = [0.0, 2.0]
+        c[:] = 1.0
+        r = generic_masked(c[:], attrs=ncd.vatts(c.name))
+        assert np.all(r.mask == False)  # noqa
+
+    os.close(fid)
+    if os.path.exists(tpath):
+        os.remove(tpath)
